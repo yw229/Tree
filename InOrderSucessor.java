6f58778bd880043afb80b5d9bf9e6e57946d0ddd
Yan Wang <yw229@njit.edu>
2012-11-02 22:59:02 -0400

    tree and related issues

---
+++ InOrderSucessor.java
@@ -0,0 +1,69 @@
+package Questions4;
+
+import java.util.Random;
+
+public class InOrderSucessor {
+	public TreeNode t ;
+	
+	public InOrderSucessor (TreeNode t)
+	{
+		this.t = t ;
+	}	
+	public InOrderSucessor ()
+	{
+		
+	}
+	public TreeNode FindNextNode(TreeNode e)
+	{
+		 //this.t = t ;
+		if (e !=null)
+		{
+		 TreeNode next ; 
+		 if ( e.parent == null || e.right != null)
+		 {
+			 next = e.mostleft( e.right) ;
+			 //return next ;
+		 }
+		 else // (e.parenet !=null && e.right == null ,)
+		 {
+			 next = e.parent ;
+			 while ( next !=null )
+			 {
+				 next = e.parent ;
+				 e = next ;
+				 if ( next.left ==e)
+					 break ;
+			 } 
+		 }
+		 return next ;
+		 
+		 //reutrn next ;
+		}
+		 return null ;
+	}
+	
+	public static void main (String[] args)
+	{
+		int ran = 10 ; 
+		int[] array = new int[ran] ;
+		Random r = new Random() ;
+		System.out.println("The array is" ) ;
+		
+		for (int i= 0; i < array.length ; i ++)
+		{
+			array[i] = r.nextInt(10) ;
+			System.out.print(" " + array[i]) ;
+		}
+		TreeNode tr = TreeNode.createMinBST(array) ;  
+		InOrderSucessor inorder = new InOrderSucessor() ;
+		TreeNode node = inorder.FindNextNode( tr) ;
+		System.out.println() ;
+		if (node != null) {
+			System.out.println(tr.data + "->" + node.data);
+		} else {
+			System.out.println(node.data + "->" + null);
+		}
+		
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+Implement a function to check if a tree is balanced. For the purposes of this question, a balanced tree is defined to be a tree such that no two leaf nodes differ in distance from the root by more than one.
+
+Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+
+Given a sorted (increasing order) array, write an algorithm to create a binary tree with minimal height.
+
+Given a binary search tree, design an algorithm which creates a linked list of all the nodes at each depth (i.e., if you have a tree with depth D, you¡¯ll have D linked lists).
+
+Write an algorithm to find the ¡®next¡¯ node (i.e., in-order successor) of a given node in a binary search tree where each node has a link to its parent.
