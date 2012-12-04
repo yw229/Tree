fd7aa81aa185c4af4152468b7a64c7fc357d4862
Yan Wang <yw229@njit.edu>
2012-12-03 23:40:53 -0500

    find LCA only given two nodes and parent pointer without root

---
+++ LCA.java
@@ -0,0 +1,76 @@
+package Questions4;
+
+import java.util.Random;
+
+public class LCA {
+	 public int heightH(TreeNode p)
+	    {
+	        
+	       /* if(p== null) return 0;
+	        if(p.parent==null) return 1 ;
+	         return 1+heightH(p.parent) ;*/
+		 int height = 0 ; 
+		 while(p!=null)
+		 {
+			 p=p.parent ;
+			 height ++ ;
+		 }
+		 return height ;
+	       // return  0 ;
+	    }
+	 public TreeNode FindLAC(TreeNode a , TreeNode b)
+	 {
+		 int ha = heightH(a) ;
+		 int hb = heightH(b) ;
+		 if(ha >hb) // assume b is deeper than a 
+		 {
+			 swap(ha,hb) ;
+			 swapNode(a,b) ;
+		 }
+		 int diff = hb - ha ; // find difference 
+		 /*
+		  * The node which is closer to root is always diff ahead of the deeper node */
+		 for(int h = 0 ; h < diff ; h ++)
+			 b= b.parent ; // b reach the same level with a
+		//return b;
+		 while(true)
+		 {
+			 if( a==b) return a ; // find the common root 
+			 a = a.parent ;
+			 b = b.parent ;
+		 }
+		 
+			
+	 }
+	private void swapNode(TreeNode a, TreeNode b) {
+		// TODO Auto-generated method stub
+		int temp = a.data ;
+		a.data =b.data ;
+		b.data=temp;
+		
+	}
+	private void swap(int ha, int hb) {
+		// TODO Auto-generated method stub
+		int temp = ha ;
+		ha = hb ;
+		hb =temp ;
+	}
+	public static void main(String[] args)
+    {
+        Random ran = new Random() ;
+        int array[] = new int[10] ;
+        for (int i=0 ; i < array.length ; i++)
+        {
+            array[i] = ran.nextInt(10) ;
+            System.out.print(" " + array[i]) ;
+        }
+        TreeNode t = TreeNode.createMinBST(array) ;
+        TreeNode a = t.left.right ;
+        TreeNode b = t.left.left ;
+        System.out.println(" TreeRoot" + t.data) ;
+        LCA lca= new LCA() ;
+        TreeNode la = lca.FindLAC(a, b) ;
+        System.out.println(" The first common of ; ancesotr of node a:" + a.data + "b:"+ b.data +
+        		"Common" + la.data) ;
+    }
+}
