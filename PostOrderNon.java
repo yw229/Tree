d4ed052dcfd03ece1982dd0c6000cc3cf9fe2e3b
Yan Wang <yw229@njit.edu>
2012-12-31 23:23:57 -0500

    post order traversal non-recursive

---
+++ PostOrderNon.java
@@ -0,0 +1,51 @@
+package Questions4;
+
+import java.util.Stack;
+
+public class PostOrderNon
+{
+    public TreeNode t ;
+    public Stack<TreeNode> s1 ;
+    public Stack<TreeNode> s2 ;
+    public PostOrderNon(TreeNode t)
+    {
+        this.t = t ; 
+        s1 = new Stack<TreeNode>() ;
+        s2 = new Stack<TreeNode>() ;
+    }
+    public void PostOrder()
+    {
+        s1.push(t) ;
+        while(!s1.isEmpty())
+        {
+            TreeNode current = s1.pop();
+            s2.push(current) ;
+            if(current.left!=null)
+                s1.push(current.right);
+            if(current.left!=null)
+                s1.push(current.left) ;
+        }
+        while(!s2.isEmpty())
+        {
+            System.out.print(" " + s2.pop().data) ;
+        }
+    }
+    public static TreeNode createBST(int[]array, int low, int high)
+    {
+    	if(low>high) return null ;
+		int mid = (low+high)/2 ;
+		TreeNode n = new TreeNode(array[mid]) ;
+		n.setLeft(createBST( array,low, mid-1) ) ;
+		n.setRight(createBST(array,mid+1,high) );
+		return n ;
+    }
+    public static void main(String[] args)
+    {
+    	int[] array = {2,3,4,5,6,7,8,9} ;
+		//TreeNode t = addToTree(array, 0, array.length-1) ;
+        TreeNode t = createBST(array,0,array.length -1) ;
+        PostOrderNon pt= new PostOrderNon(t) ;
+        pt.PostOrder() ;
+        
+    }
+}
