a2abe289bfc38e033b3c4a1166b419d682a22957
yw229 <yw229@njit.edu>
2014-08-14 17:28:27 -0400

    LevelOrderTraversalWithDepth

--- LevelOrderTraversal.py
+++ LevelOrderTraversal.py
@@ -18,6 +18,43 @@ def leveOrderTravi(root):
 				l.append(out.right)
 		
 		print(out.data)
+
+def levelOrderTravList(root):
+	l=[]
+	levell = [] #for each level of the tree, use list to store the nodes 
+  	levell.append(root) 
+	l.append(levell)
+	dep = 0 	
+	while True:
+			currL = l[dep] # get current level nodes
+			dlist = [] #create new list to store curr level nodes left/right nodes 
+
+			for e in currL:
+				if e:
+					print ('current node is %s' %e.data)
+					
+					if e.left:
+						print("l child value is %s: "%e.left.data)
+						dlist.append(e.left)
+					if e.right:
+						print("r child value is %s: " %e.right.data)
+						dlist.append(e.right)
+					
+		
+			#must check whether the current level is the leaf level 
+			if len(dlist)>0 :		
+				l.insert(dep+1, dlist)
+				print(len(dlist))
+			else:
+				break
+							
+			
+			dep +=1 
+			
+			print("depth is %s"%dep)	
+	
+	return l
+		
 			
 if __name__=="__main__":
 		bTree = BTree()
@@ -34,5 +71,14 @@ if __name__=="__main__":
 		print("after level order traversal : \n") 
 	
 		leveOrderTravi(root)	
+		
+		print 
+		
+		print("print each level nodes : \n")
+		
+		l=levelOrderTravList(root)
 
+		print(len(l))
+		
+		
 
